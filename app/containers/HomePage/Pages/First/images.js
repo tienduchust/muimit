849c123c5668941443881cc31ab100ce7ad56267
ductt <ductt.hn@havaz.vn>
2020-05-05 10:38:43 +0700

    dfdf

--- app/containers/HomePage/Pages/First/images.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-webpack-loader-syntax */
-import '!file-loader?name=[name].[ext]&outputPath=./images/!images/First/IMG_01.JPG';
-import '!file-loader?name=[name].[ext]&outputPath=./images/!images/First/IMG_02.JPG';
-import '!file-loader?name=[name].[ext]&outputPath=./images/!images/First/IMG_03.JPG';
-import '!file-loader?name=[name].[ext]&outputPath=./images/!images/First/IMG_04.JPG';
-import '!file-loader?name=[name].[ext]&outputPath=./images/!images/First/IMG_05.JPG';
-import '!file-loader?name=[name].[ext]&outputPath=./images/!images/First/IMG_06.JPG';
-import '!file-loader?name=[name].[ext]&outputPath=./images/!images/First/IMG_07.jpeg';
-import '!file-loader?name=[name].[ext]&outputPath=./images/!images/First/IMG_08.JPG';
