a6ccaedf7f2010cce3654e21b556c995494fb313
ductt <ductt.hn@havaz.vn>
2020-05-07 15:05:55 +0700

    dfdf

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -2,11 +2,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
-const OfflinePlugin = require('offline-plugin');
 const { HashedModuleIdsPlugin } = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = require('./webpack.base.babel')({
   mode: 'production',
 
@@ -18,8 +17,8 @@ module.exports = require('./webpack.base.babel')({
 
   // Utilize long-term caching by adding content hashes (not compilation hashes) to compiled assets
   output: {
-    filename: '[name].[chunkhash].js',
-    chunkFilename: '[name].[chunkhash].chunk.js',
+    filename: 'static/js/[name].[contenthash:8].js',
+    chunkFilename: 'static/js/[name].[contenthash:8].chunk.js',
   },
 
   optimization: {
@@ -29,11 +28,23 @@ module.exports = require('./webpack.base.babel')({
         terserOptions: {
           warnings: false,
           compress: {
+            ecma: 5,
+            warnings: false,
+            // Disabled because of an issue with Uglify breaking seemingly valid code:
+            // https://github.com/facebook/create-react-app/issues/2376
+            // Pending further investigation:
+            // https://github.com/mishoo/UglifyJS2/issues/2011
             comparisons: false,
+            // Disabled because of an issue with Terser breaking valid code:
+            // https://github.com/facebook/create-react-app/issues/5250
+            // Pending futher investigation:
+            // https://github.com/terser-js/terser/issues/120
+            inline: 2,
           },
-          parse: {},
+          parse: { ecma: 8 },
           mangle: true,
           output: {
+            ecma: 5,
             comments: false,
             ascii_only: true,
           },
@@ -46,7 +57,7 @@ module.exports = require('./webpack.base.babel')({
     nodeEnv: 'production',
     sideEffects: true,
     concatenateModules: true,
-    runtimeChunk: 'single',
+    runtimeChunk: true,
     splitChunks: {
       chunks: 'all',
       maxInitialRequests: 10,
@@ -58,7 +69,7 @@ module.exports = require('./webpack.base.babel')({
             const packageName = module.context.match(
               /[\\/]node_modules[\\/](.*?)([\\/]|$)/,
             )[1];
-            return `npm.${packageName.replace('@', '')}`;
+            return `muimit.${packageName.replace('@', '')}`;
           },
         },
       },
@@ -83,30 +94,36 @@ module.exports = require('./webpack.base.babel')({
       },
       inject: true,
     }),
-
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'static/css/[name].[contenthash:8].css',
+      chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
+    }),
     // Put it in the end to capture all the HtmlWebpackPlugin's
     // assets manipulations and do leak its manipulations to HtmlWebpackPlugin
-    new OfflinePlugin({
-      relativePaths: false,
-      publicPath: '/',
-      appShell: '/',
+    // new OfflinePlugin({
+    //   relativePaths: false,
+    //   autoUpdate: false,
+    //   publicPath: '/',
+    //   appShell: '/',
 
-      // No need to cache .htaccess. See http://mxs.is/googmp,
-      // this is applied before any match in `caches` section
-      excludes: ['.htaccess'],
+    //   // No need to cache .htaccess. See http://mxs.is/googmp,
+    //   // this is applied before any match in `caches` section
+    //   excludes: ['.htaccess'],
 
-      caches: {
-        main: [':rest:'],
+    //   caches: {
+    //     main: [':rest:'],
 
-        // All chunks marked as `additional`, loaded after main section
-        // and do not prevent SW to install. Change to `optional` if
-        // do not want them to be preloaded at all (cached only when first loaded)
-        additional: ['*.chunk.js'],
-      },
+    //     // All chunks marked as `additional`, loaded after main section
+    //     // and do not prevent SW to install. Change to `optional` if
+    //     // do not want them to be preloaded at all (cached only when first loaded)
+    //     additional: ['*.chunk.js'],
+    //   },
 
-      // Removes warning for about `additional` section usage
-      safeToUseOptionalCaches: true,
-    }),
+    //   // Removes warning for about `additional` section usage
+    //   safeToUseOptionalCaches: true,
+    // }),
 
     new CompressionPlugin({
       algorithm: 'gzip',
@@ -116,16 +133,16 @@ module.exports = require('./webpack.base.babel')({
     }),
 
     new WebpackPwaManifest({
-      name: 'React Boilerplate',
-      short_name: 'React BP',
-      description: 'My React Boilerplate-based project!',
+      name: 'MUI MIT',
+      short_name: 'MM',
+      description: 'MUI MIT MAT ONG!',
       background_color: '#fafafa',
       theme_color: '#b1624d',
       inject: true,
       ios: true,
       icons: [
         {
-          src: path.resolve('app/images/icon-512x512.png'),
+          src: path.resolve('app/images/favicon.ico'),
           sizes: [72, 96, 128, 144, 192, 384, 512],
         },
         {
